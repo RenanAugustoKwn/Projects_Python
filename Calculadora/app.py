6ba65b6e3d723e0313108bacce468683271e2c50
RenanAugustoKwn <renanaugustokwn@outlook.com>
2025-02-04 23:50:07 -0300

    Update app.py

--- Calculadora/app.py
+++ Calculadora/app.py
@@ -9,6 +9,7 @@ cor3 = "#bbbcbd"
 janela = Tk()
 janela.title("Calculadora")
 janela.geometry("235x318")
+janela.resizable(False, False)
 janela.config(bg=cor1)
 
 frame_tela = Frame(janela, width=235, height=50, bg=cor2)
@@ -19,7 +20,13 @@ frame_corpo.grid(row=1, column=0)
 
 # Craindo Botoes
 
-b_1 = Button(frame_corpo, text= "Clean", width=11, height=2)
-b_1.place(x=0,y=0)
+b_1 = Button(frame_corpo, text="Clean", width=18, height=2, overrelief=RIDGE)
+b_1.place(x=0, y=0)
+
+b_2 = Button(frame_corpo, text="%", width=5, height=2, overrelief=RIDGE)
+b_2.place(x=140, y=0)
+
+b_3 = Button(frame_corpo, text="/", width=5, height=2, overrelief=RIDGE)
+b_3.place(x=190, y=0)
 
 janela.mainloop()
